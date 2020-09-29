15bbdad3845104309ca17e284970645acfe86253
Junior <junior_tapia_sonic@hotmail.com>
2020-09-29 15:03:35 -0500

    Version 8

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/SLA',{
+mongoose.connect('mongodb+srv://junior:junior123>@proyectobd3.l4goe.mongodb.net/pbd3?retryWrites=true&w=majority',{
     //ponemos configuraciones para que el modulo si se actualiza no haya problemas cuando estes haciendo consultas a tu bd 
     useCreateIndex: true,
     useNewUrlParser: true,
